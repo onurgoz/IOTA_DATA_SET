1ab7f85ef41bbbf29a14613db165c788950016f2
baha908 <182802017@ogr.cbu.edu.tr>
2020-11-17 14:57:34 +0300

    sadeleştirme yapıldı

--- IOTA_DATA.py
+++ IOTA_DATA.py
@@ -8,20 +8,10 @@ headers_param = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWe
 #Verileri çekeceğim web sayfası
 Link="https://www.coinlore.com/coin/iota/historical-data/-7200/1605301199#hist-prices"
 
-#Verileri ayırırken kullanıcağımız diziler
-Date_IOTA=['Date']
-Open_IOTA=['Open']
-High_IOTA=['High']
-Low_IOTA=['Low']
-Close_IOTA=['Close']
-Volume_=['Volume']
-Volume_IOTA=['Volume IOTA']
-Market_Cap_IOTA=['Market Cap']
-IOTA_DATA=[]
-
 #Dosya işlemleri
-Data_xlm = Workbook()
-sayfa = Data_xlm.active
+Data_xlsx = Workbook()
+sayfa1=Data_xlsx.create_sheet("İslenmemis Veri",0)
+sayfa2=Data_xlsx.create_sheet("İslenmis Veri",1)
 
 
 #Veriyi parselleme
@@ -30,28 +20,22 @@ soup = BeautifulSoup(IOTA_page.content,"html.parser")
 
 #Veriyi ayıklama
 ozellikler= list(soup.find_all("td",attrs={"class":"no-wrap"}))
-print(ozellikler)
+
+#İşlenmemiş verimizi kaydediyoruz
+for i in range(0,len(ozellikler)):
+  sayfa1.append([ozellikler[i].text])
 
 #Verideki $ işaretlerini temizliyoruz
 for i in range(0,len(ozellikler)):
   ozellikler[i]=ozellikler[i].text.replace("$","")
 
-#Elimizdeki veriyi parçalıyoruz
-for i in range(0,9863,8):
-   Date_IOTA.append(ozellikler[0+i])
-   Open_IOTA.append(ozellikler[1+i])
-   High_IOTA.append(ozellikler[2+i])
-   Low_IOTA.append(ozellikler[3+i])
-   Close_IOTA.append(ozellikler[4+i])
-   Volume_.append(ozellikler[5+i])
-   Volume_IOTA.append(ozellikler[6+i])
-   Market_Cap_IOTA.append(ozellikler[7+i])
-   
+#Elimizdeki veriyi parçalayıp xlsx dosyasına yazıyoruz
 
-#Parçaladığımız veriyi birleştiriyoruz
-for d,o,h,l,c,v,vi,m in zip(Date_IOTA,Open_IOTA,High_IOTA,Low_IOTA,Close_IOTA,Volume_,Volume_IOTA,Market_Cap_IOTA):
-    sayfa.append([d,o,h,l,c,v,vi,m])
+sayfa2.append(['Date','Open','High','Low','Close','Volume','Volume IOTA','Market Cap'])
+
+for i in range(0,9863,8):
+   sayfa2.append([ozellikler[0+i],ozellikler[1+i],ozellikler[2+i],ozellikler[3+i],ozellikler[4+i],ozellikler[5+i],ozellikler[6+i],ozellikler[7+i]])
 
 #Dosyayı kaydedip kapatıyoruz
-Data_xlm.save("IOTA_DATA.xlsx")
-Data_xlm.close()
+Data_xlsx.save("IOTA_DATA.xlsx")
+Data_xlsx.close()
